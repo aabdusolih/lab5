6060192c0fa6eddf958e0539afe422f9ab4bfbb8
abdusol1h <abdusolix11@gmail.com>
2025-02-12 19:15:19 +0500

    v1

---
+++ main.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+using namespace std;
+
+/*  problem 1
+int main(){
+    float subproblem, avg;
+    float n;
+    int sum;
+    cout<<"Enter subproblem number: ";
+    cin>>subproblem;
+    int sub = subproblem * 10;
+    switch(sub){
+    case 11:
+    for (int i = 1; i <= 10; i++){
+      cout << i<< " ";
+    }
+    break;
+    case 12:
+    cout << "Enter a natural number: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++){
+        cout << i << " ";
+    }
+    break;
+
+    case 13:
+    for (int i = 1; i <= 10; i++){
+        sum += i;
+    }
+    cout << sum;
+    break;
+
+    case 14:
+    cout << "Enter a natural number: ";
+    cin >> n;
+    for (int i = 0; i <= n; i++){
+        sum += i;
+    }
+    cout << sum;
+    break;
+
+    case 15:
+    for (int i = 1; i <= 10; i++){
+        sum += i;
+    }
+    avg = sum / 10.0;
+    cout << avg;
+    break;
+
+    case 16:
+    cout << "Enter a natural number: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++){
+        sum += i;
+    }
+    avg = sum / n;
+    cout << avg;
+    break;
+
+    default:
+        cout<<"Invalid input.";
+    }
+    return 0;
+}*/
+
+/*  problem 2
+main(){
+    float subproblem;
+    int n;
+    cout << "Enter the subproblem number: ";
+    cin >> subproblem;
+    int sub = subproblem * 10;
+    switch (sub){
+    case 21:
+        for (int i=0; i<=10; i++){
+            if (i%2==1){
+                cout << i << " ";
+            }
+        }
+    break;
+
+    case 22:
+    for (int i=0; i<=10; i++){
+    if (i%2==0){
+        cout << i << " ";
+    }
+    }
+    break;
+
+    case 23:
+    cout << "Enter a natural number: ";
+    cin >> n;
+    for (int i=0; i<=n; i++){
+        if (i%2==1){
+            cout << i << " ";
+    }
+    }
+    break;
+
+    case 24:
+    cout << "Enter a natural number: ";
+    cin >> n;
+    for (int i=0; i<=n; i++){
+        if (i%2==0){
+            cout << i << " ";
+    }
+    }
+    break;
+
+    case 25:
+    for (int i=0; i<=100; i++){
+        if (i%10==0){
+            cout << i << " ";
+        }
+    }
+    break;
+
+    case 26:
+    cout << "Enter a natural number: ";
+    cin >> n;
+    for (int i=0; i<=n; i++){
+        if (i%10==0){
+            cout << i << " ";
+    }
+    }
+    break;
+
+    default:
+    cout << "Wrong input.";
+    }
+    return 0;
+}*/
